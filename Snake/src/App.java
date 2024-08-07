8932fbe0b58ef8f4480b619fd1c3dcca0b940b03
Leo <leo.haanpaa@edu.tampere.fi>
2024-08-07 17:16:39 +0300

    Started a project Snake Game

---
+++ Snake/src/App.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 600;
+        int boardHeight = boardWidth;
+
+        JFrame frame = new JFrame("Snake");
+        
+        
+    }
+}
