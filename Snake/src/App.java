1238b2d8a1f214d06aba90894e095654c3b8d7c3
Leo <leo.haanpaa@edu.tampere.fi>
2024-08-07 18:06:34 +0300

    Added frame for the program

--- Snake/src/App.java
+++ Snake/src/App.java
@@ -1,12 +1,20 @@
-import javax.swing.*;
+import javax.swing.*; //creates the window for the game
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int boardWidth = 600;
-        int boardHeight = boardWidth;
+        int boardWidth = 600;  //defines the width of the window
+        int boardHeight = boardWidth; //defines the height of the window(copies it from the width)
 
-        JFrame frame = new JFrame("Snake");
+        JFrame frame = new JFrame("Snake"); //creates the frame (name) for the program
+        frame.setVisible(true); //makes the frame visible
+        frame.setSize(boardWidth, boardHeight); //defines the size of the frame
+        frame.setLocation(null); //opens the window in the middle of the screen
+        frame.setResizable(false); //makes the window unable to be resized
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //makes the program terminate when close (x) is pressed
         
+
+        SnakeGame snakeGame = new SnakeGame(boardWidth, boardHeight);
+        frame.add(snakeGame);        
         
     }
 }
